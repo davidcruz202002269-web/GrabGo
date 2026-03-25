e9813a888f6c65b7244e57aaf0fbadde0dac5c90
Jose Michelle Jimenez <josemichelle0331@gmail.com>
2026-03-25 15:54:26 -0400

    Agregue las aplicaciones a cada uno de los archivos backend

---
+++ backend/apiGateway/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import bcrypt from "bcrypt";
+import config from "dotenv/config";
+import jwt from "jsonwebtoken";
+import db from './src/db/usuariosRegistrados.json' with { type: 'json' };
+
+config;
+const app = express();
+const port = process.env.PORT;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/src')));
+
+app.get('/', (req, res) => {
+    res.send('Products');
+});
+
+app.listen(port, () => {
+    console.log(`Products esta ejecutandose... http://localhost:${port}`);
+});
+
+console.log(db);
