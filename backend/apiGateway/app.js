de8bf405e281273af34e56e0956a0f4e89f686f5
Jose Michelle Jimenez <josemichelle0331@gmail.com>
2026-03-26 14:51:28 -0400

    Importe el router de auth de registro y lo llame en el evento http post

--- backend/apiGateway/app.js
+++ backend/apiGateway/app.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from "url";
 import bcrypt from "bcrypt";
 import config from "dotenv/config";
 import jwt from "jsonwebtoken";
+import { routerAuth } from "../auth/routes/auth.routes.js";
 
 config;
 const app = express();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../../frontend/public')));
 
+app.post('/auth/register', routerAuth);
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../frontend/public/index.html'));
 });
