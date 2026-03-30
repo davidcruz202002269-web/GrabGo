588e6f0d56aed0c8b9eebaee0fedfe829d67d08f
Carlos Acosta Rosario <carlosacosta0637@gmail.com>
2026-03-29 21:51:22 -0400

    Hice que el valor del input fuera mas legible y fácil de manejar

---
+++ frontend/public/js/tarjeta.js
@@ -0,0 +1,18 @@
+const inputNumeracion = document.querySelector('#numeracion');
+
+inputNumeracion.addEventListener('input', (tecla) => {
+
+    let valor = tecla.target.value.replace(/\s+/g, '').replace(/[^0-9]/gi, '');
+    let valorFormateado = '';
+
+    for (let i = 0; i < valor.length; i++) {
+        if (i > 0 && i % 4 === 0) {
+            valorFormateado += ' ';
+        }
+        valorFormateado += valor[i];
+
+    }
+
+    tecla.target.value = valorFormateado;
+});
+
